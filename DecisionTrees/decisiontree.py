f74bba06859bb4f61be12e20b745d59470f66ae9
unknown <nitinreddy.k@gmail.com>
2019-02-19 13:15:25 -0500

    adding pruning DT

--- DecisionTrees/decisiontree.py
+++ DecisionTrees/decisiontree.py
@@ -1,92 +1,125 @@
+# Importing the required packages for the assignment
 import numpy as np
 import math
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 from tqdm import tqdm
+from statistics import mode
 
-def data_generator(m,k):
-    #Generating the X
+def data_generator(m):
+    """
+    Input : 
+        m - Number of samples
+    
+    Output : 
+        Dataframe with k+1 columns i.e. k features that represent X 
+        and 1 the represents Y
+    """
+    colnames = [ 'X'+ str(i)  for i in range(21)]
+    
     X = [] 
     for j in range(m):
         y = []
         a = np.random.binomial(1, p=0.5, size= 1)[0]
         y.append(a)
-        for i in range(k-1):
+        for i in range(14):
             x = np.random.binomial(1, p=0.75, size= 1)[0]
             if x == 1:
                 y.append(a)
             else:
                 y.append(1-a)
                 a = 1-a
+        for i in range(6):
+            x = np.random.binomial(1, p = 0.5, size = 1)[0]
+            y.append(x)
         X.append(y)
-        
-    #Generating the Weights
-    j = 0.9*0.9
-    s = j * (1 - (0.9 ** (k-1)))/ (1- 0.9)
-    w = [0]
-    for i in range(k-1):
-        w_i = 0.9**(i+2)/ s
-        w.append(w_i)
+     
+    df = pd.DataFrame(data= X, columns= colnames)
     
     #Generating the Y
     Y = []
-    for i in range(m):
-        y_i = [a*b for a,b in zip(X[i],w)]
-        if sum(y_i) >= 0.5:
-            Y.append(X[i][0])
-        else:
-            Y.append(1-X[i][0])
+    for i in X:
+        if i[0] == 0:
+            y = mode(i[1:8])
+        else :
+            y = mode(i[8:15])
+        Y.append(y)
     
     #Generate column names
-    colnames = [ 'X'+ str(i)  for i in range(1,k+1)]
-    
-    df = pd.DataFrame(data= X, columns= colnames)
     df['Y'] = pd.Series(data = Y)
     return(df)
 
 
-data = pd.read_csv("E:\Semester - 2\MachineLearning\Decision Trees\data.csv")
-
-
 class Node():
+    """
+    Node is a data structure which will be used for decision trees.
+    
+    Input :
+        data = training data post split is stored
+        rule = feature on which the split led to this node and the 
+        value of the feature
+        child = nodes of children of all this node are present 
+        after the split
+    """
     def __init__(self,
                  data = None,
                  rule = None,
-                 error = None,
-                 child = None
+                 child = None,
+                 depth = None
                 ):
         self.data = data
         self.rule = rule
-        self.error = error
         self.child = child
+        self.depth = depth
 
 
-
-
-class Decision_Tree():
-    
-    def __init__(self, root = None):
+class Decision_Tree_ID3():
+    """
+    Decision Tree ID3 is trained on data with a target variable. It 
+    is built on split variable which is indentified using the logic 
+    of information gain 
+    """
+    def __init__(self, root = None, termination_depth = None, min_leaf_size = 4, sig_threshold = None):
         self.root = root
+        self.termination_depth = termination_depth
+        self.min_leaf_size = min_leaf_size
+        self.sig_threshold = sig_threshold
         
-        
-    def entropy(self, data, variable):
-        # Calculate the entropy of target variable
-        entropy_value = sum([(-data[variable].value_counts()[i]/ data[variable].count()) * np.log2((data[variable].value_counts()[i]/ data[variable].count()) + 0.00000001) for i in data[variable].unique()])
+    def _entropy(self, data, variable):
+        """
+        Calcuates the entropy for the given data and target variable
+        """
+        entropy_value = sum([(-data[variable].value_counts()[i]/ 
+                              data[variable].count()) * np.log2((
+            data[variable].value_counts()[i]/ 
+            data[variable].count()) + 0.00000001) 
+                             for i in data[variable].unique()])
         return entropy_value
     
     
-    def information_gain(self, data, variable, target):
-        infomation_content = sum([data[variable].value_counts()[i]/data[variable].count() * self.entropy(data[data[variable]== i], target) for i in data[variable].unique()])
-        info_gain = self.entropy(data, target) - infomation_content
+    def _information_gain(self, data, variable, target):
+        """
+        Calculates the information gain for the given variable and data
+        """
+        infomation_content = sum([data[variable].value_counts()[i]/
+                                  data[variable].count()
+                                  * self._entropy(data[data[variable]== i], 
+                                                 target) 
+                                  for i in data[variable].unique()])
+        info_gain = self._entropy(data, target) - infomation_content
         return(info_gain)
     
     
-    def split_variable_identification(self, data, target):
+    def _split_variable_identification(self, data, target):
+        """
+        Identifies the split variable based on data and target
+        """
         #loop through all features and calculate information gain for each feature
         variable_ig_required = list(data.columns)
         variable_ig_required.remove('Y')
-        ig_values = [(i, self.information_gain(data,i,'Y')) for i in variable_ig_required]
+        ig_values = [(i, self._information_gain(data,i,'Y')) 
+                     for i in variable_ig_required]
         if len(ig_values) != 0:
             split_variable = max(ig_values, key = lambda item : item[1])
         else:
@@ -94,120 +127,15 @@ class Decision_Tree():
         return(split_variable)
     
 
-    def split_data(self, data, split_variable): 
-        splitted_data = [Node(data = (data[data[split_variable] == i].drop(split_variable,1)), rule = (split_variable,i)) for i in data[split_variable].unique()]
-        return(splitted_data)
-    
-    
-    def fit(self, data, target):
-        
-        if type(data) != Node:
-            data = Node(data = data)
-            self.root = data
-
-        if self.split_variable_identification(data.data, target)[1] == 0:
-            return
-
-        split_variable = self.split_variable_identification(data.data, target)[0]
-        data.child = self.split_data(data.data, split_variable)
-
-        for i in data.child:
-            if i.data['Y'].nunique() == 2:
-                self.fit(i, target)
-            
-    
-    def get_rules(self, model = None ,ruleList = []):
-        
-        if model == None:
-            model = self.root
-        
-        ruleList.append(model.rule)
-
-        if model.child == None:
-            ruleList.append(model.data['Y'].mode()[0])
-            return print(ruleList[1:])
-
-        for i in model.child:
-            self.get_rules(i,ruleList.copy())
-    
-
-    def predict_row(self, model, row):
-        
-        if model.child == None:
-            return(model.data['Y'].mode()[0])
-
-        variable = model.child[0].rule[0]
-        row_value = row[variable]
-        for i in model.child:
-            if i.rule[1] == row_value:
-                return self.predict_row(i, row)
-            
-    def predict(self, test):
-        predicted_y = []
-        for i in test.iterrows():
-            x = i[1]
-            y = self.predict_row(self.root, x)
-            predicted_y.append(y)
-        return pd.Series(predicted_y)
-    
-    def training_error(self):
-        predict_train = self.predict(data)
-        return (1 -sum(data['Y'] == predict_train)/ len(data))
-            
-        
-
-
-
-
-
-
-
-class Decision_Tree_Gini_Impurity():
-    """
-    Decision Tree Gini Impurity is trained on data with a target variable. It is built on split variable which is indentified using the logic of Gini Impurity.
-    """
-    def __init__(self, root = None):
-        self.root = root
-        
-        
-    def gini_index(self, data, variable):
+    def _split_data(self, data, split_variable): 
         """
-        Calcuates the gini index for the given data and target variable
+        Splits the data after identifying the split variable, assigns 
+        data and rule to the node.
         """
-        # Calculate the entropy of target variable
-        gini_index = 1- sum([pow(data[variable].value_counts()[i]/ data[variable].count(),2) for i in data[variable].unique()])
-        return gini_index
-    
-    
-    def gini_gain(self, data, variable, target):
-        """
-        Calculates the gini impurity for the given variable and data
-        """
-        gini_index_varaible = sum([data[variable].value_counts()[i]/data[variable].count() * self.gini_index(data[data[variable]== i], target) for i in data[variable].unique()])
-        gini_gain_value = gini_index_varaible
-        return(gini_gain_value)
-    
-    
-    def split_variable_identification(self, data, target):
-        """
-        Identifies the split variable based on data and target
-        """
-        #loop through all features and calculate gini gain for each feature
-        variable_ig_required = list(data.columns)
-        variable_ig_required.remove('Y')
-        ig_values = [(i, self.gini_gain(data,i,'Y')) for i in variable_ig_required]
-        if len(ig_values) != 0:
-            split_variable = min(ig_values, key = lambda item : item[1])
-        else:
-            split_variable = (0,0)
-        return(split_variable)
-    
-
-    def split_data(self, data, split_variable): 
-        """
-        Splits the data after identifying the split variable, assigns data and rule to the node.
-        """
-        splitted_data = [Node(data = (data[data[split_variable] == i].drop(split_variable,1)), rule = (split_variable,i)) for i in data[split_variable].unique()]
+        splitted_data = [Node(data = (data[data[split_variable] == i].
+                                      drop(split_variable,1)),
+                              rule = (split_variable,i)) 
+                         for i in data[split_variable].unique()]
         return(splitted_data)
     
     
@@ -216,17 +144,24 @@ class Decision_Tree_Gini_Impurity():
         Fit is used to fit decision trees on the data for a given target variable
         """
         if type(data) != Node:
-            data = Node(data = data)
+            data = Node(data = data, depth = 0)
             self.root = data
-
-    #    if self.split_variable_identification(data.data, target)[1] == 0:
-    #        return
-
-        split_variable = self.split_variable_identification(data.data, target)[0]
-        data.child = self.split_data(data.data, split_variable)
-
+        
+        #Terminating Conditions
+        if self._split_variable_identification(data.data, target)[1] == 0 :
+            return
+        if data.depth == self.termination_depth:
+            return
+        if data.data.shape[0] <= self.min_leaf_size:
+            return
+        
+        split_variable = self._split_variable_identification(data.data, target)[0]
+        data.child = self._split_data(data.data, split_variable)
         for i in data.child:
-            if i.data['Y'].nunique() != 1: 
+            i.depth = data.depth + 1
+               
+        for i in data.child:
+            if i.data['Y'].nunique() != 1:
                 self.fit(i, target)
             
     
@@ -237,18 +172,35 @@ class Decision_Tree_Gini_Impurity():
         if model == None:
             model = self.root
         ruleList.append(model.rule)
-
         if model.child == None:
             ruleList.append(model.data['Y'].mode()[0])
             return print(ruleList[1:])
-
         for i in model.child:
             self.get_rules(i,ruleList.copy())
+            
+    def get_irrelevant_variable(self, irrelevant_variables, model = None , var = []):
+        """
+        Returns the count of irrelevant variables present in the decision tree
+        """
+        if model == None:
+            model = self.root
+
+        if model.child == None:
+            return
+
+        for i in model.child:
+            if i.rule[0] in irrelevant_variables:
+                var.append(i.rule[0])
+                print(var)
+            self.get_irrelevant_variable(irrelevant_variables,i,var)
+        
+        irr_var = list(set(var))
     
 
-    def predict_row(self, model, row):
+    def _predict_row(self, model, row):
         """
-        This function returns the prediction for the a single sample of data using the fitted data
+        This function returns the prediction for the a single sample of 
+        data using the fitted data
         """
         if model.child == None:
             return(model.data['Y'].mode()[0])
@@ -257,16 +209,17 @@ class Decision_Tree_Gini_Impurity():
         row_value = row[variable]
         for i in model.child:
             if i.rule[1] == row_value:
-                return self.predict_row(i, row)
+                return self._predict_row(i, row)
             
     def predict(self, test):
         """
-        Predict funtion will take an input data and return the prediction based on the fitted decision tree
+        Predict funtion will take an input data and return the prediction
+        based on the fitted decision tree
         """
         predicted_y = []
         for i in test.iterrows():
             x = i[1]
-            y = self.predict_row(self.root, x)
+            y = self._predict_row(self.root, x)
             predicted_y.append(y)
         return pd.Series(predicted_y)
     
@@ -276,11 +229,12 @@ class Decision_Tree_Gini_Impurity():
         """
         predict_train = self.predict(data)
         return (1 -sum(data['Y'] == predict_train)/ len(data))
-            
 
 
-model = Decision_Tree_Gini_Impurity()
 
-model.fit(data, 'Y')
+data = data_generator(1000)
+tree = Decision_Tree_ID3()
+tree.fit(data, 'Y')
 
-print(model.training_error())
+irrelevant_variables = ['X15','X16','X17','X18','X19','X20']
+tree.get_irrelevant_variable(irrelevant_variables)
